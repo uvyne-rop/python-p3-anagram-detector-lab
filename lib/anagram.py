adb9b96ae317981c2731b7e3bd0a3b21c335e1d3
uvyne-rop <ropchiri7@gmail.com>
2024-05-27 11:21:35 +0300

    initial changes

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        sorted_word = sorted(self.word)
+        return [word for word in words if sorted(word) == sorted_word]
